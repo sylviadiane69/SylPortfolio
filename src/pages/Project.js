545a802b4a5ed6634e57e560361ef5d9607cc048
Sylvia Soto <sylviadiane69@gmail.com>
2020-06-13 18:53:17 -0500

    changes to files

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,32 +1,34 @@
 // import React from "react";
-// import Wrapper from "./components/Wrapper";
-// import Header from "./components/Header";
-// import Projects from "./components/Projects";
+// import Wrapper from ".Wrapper/components/Wrapper";
+// // import Header from "./components/Header";
+// import Project from "./pages/Project";
 // import projects from "./projects.json";
 
 // function App() {
 //   return (
+//     <div>
 //     <Wrapper>
-//       <Header>Projects</Header>
-//       <Projects
+//       {/* <Header>Projects</Header> */}
+//       <Project
 //         name={projects[0].name}
 //         image={projects[0].image}
 //         occupation={projects[0].occupation}
 //         location={projects[0].location}
 //       />
-//       <Projects
+//       <Project
 //         name={projects[1].name}
 //         image={projects[1].image}
 //         occupation={projects[1].occupation}
 //         location={projects[1].location}
 //       />
-//       <Projects
+//       <Project
 //         name={projects[2].name}
 //         image={projects[2].image}
 //         occupation={projects[2].occupation}
 //         location={projects[2].location}
 //       />
 //     </Wrapper>
+//     </div>
 //   );
 // }
 
